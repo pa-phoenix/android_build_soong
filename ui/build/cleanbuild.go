0ae0233b076b07993e725aa0c34edeb0ff193571
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-06-27 09:45:54 +0530

    soong: Nuke kernel stuff on installclean
    Change-Id: I4e6118954d0865de0feba18090f523a33fca213c

--- ui/build/cleanbuild.go
+++ ui/build/cleanbuild.go
@@ -110,6 +110,8 @@ func installClean(ctx Context, config Config, what int) {
 		productOut("kernel"),
 		productOut("data"),
 		productOut("skin"),
+		productOut("obj/kernel"),
+		productOut("obj/KERNEL_OBJ"),
 		productOut("obj/NOTICE_FILES"),
 		productOut("obj/PACKAGING"),
 		productOut("ramdisk"),
